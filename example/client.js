f7c6607e7ee9af05a0db636a3bca7aa1c37a32c5
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-12 13:26:02 +0700

    update example to use widget

--- example/client.js
+++ example/client.js
@@ -2,7 +2,7 @@
 var RText     = require('..')
 var reconnect = require('reconnect')
 var reloader  = require('client-reloader')
-
+var widget    = require('../widget')
 var ta = document.getElementById('ta')
 
 var rText = RTEXT = RText().wrap(ta)
@@ -11,3 +11,8 @@ reconnect(reloader(function (stream) {
   stream.pipe(rText.createStream()).pipe(stream)
 })).connect('/shoe')
 
+
+console.log(widget)
+
+//using the default template...
+document.body.appendChild(widget(rText))
