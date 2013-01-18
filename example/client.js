52c107664dd036740e306eacf67e5e3068a008bb
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-18 22:39:51 +0700

    simplify client.js

--- example/client.js
+++ example/client.js
@@ -3,13 +3,13 @@ var RText     = require('..')
 var reconnect = require('reconnect')
 var reloader  = require('client-reloader')
 var widget    = require('../widget')
-var ta = document.getElementById('ta')
 
-var rText = RTEXT = RText().wrap(ta)
+var rText = RTEXT = RText()
 
 reconnect(reloader(function (stream) {
   stream.pipe(rText.createStream()).pipe(stream)
 })).connect('/shoe')
 
 //using the default template...
+document.body.appendChild(rText.widget())
 document.body.appendChild(widget(rText))
