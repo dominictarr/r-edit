ddb1dc47f97945c32a7af2da9eb58661539e9506
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-18 22:12:49 +0700

    update client

--- client.js
+++ client.js
@@ -1,10 +1,30 @@
 var widget = require('./widget')
 
-require('rumours')({
+var hashChange = require('hash-change').on('change', open)
+
+var rumours = require('rumours')({
   host: 'http://rumoursdb.com:4567',
   db: 'r-edit-demo'
-}).open('r-edit_demo', function (err, rEdit) {
-  if(err) throw err
-  rEdit.wrap(document.getElementById('ta'))
-  document.body.appendChild(widget(rEdit))
 })
+
+var doc
+
+function open(hash) {
+  if(doc) doc.dispose()
+
+  rumours.open('r-edit_demo', function (err, rEdit) {
+    doc = rEdit
+    if(err) throw err
+    document.body.innerHTML = ''
+    var ta = document.createElement('textarea')
+    ta.setAttribute('cols', 80)
+    ts.setAttribute('rows', 24)
+    document.body.appendChild(ta)
+    rEdit.wrap(ta)
+    document.body.appendChild(widget(rEdit))
+
+  })
+}
+
+open(hashchange.hash())
+
